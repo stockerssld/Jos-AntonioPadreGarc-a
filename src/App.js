712e0af7d64a9ab30d2830ce975f9502734a5998
stockerssld <35477074+stockerssld@users.noreply.github.com>
2020-05-30 22:04:09 -0500

    add footer 1

--- src/App.js
+++ src/App.js
@@ -5,22 +5,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{flex:1, backgroundColor:"red"}}>
+        s
+      </div>
+      <div style={{flex:1, backgroundColor:'blue'}}>
+        w
+      </div>
+      <div style={{flex:1, backgroundColor: 'yellow'}}>
+        sp
+      </div>
+      <Footer/>
     </div>
   );
 }
 
+const Footer=()=>(
+  <div style={{flex:1, backgroundColor:'#000'}}>
+    <p style={{color:'white'}}>
+      © José Antonio Padre García ICP netlify
+    </p>
+  </div>
+)
 export default App;
